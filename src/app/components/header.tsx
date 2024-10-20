c29bb5c3471d069e429453d5ed9d5070500088d7
abilash <abilashb2000@gmail.com>
2024-10-20 21:32:57 +0530

    added siebar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,6 @@
+"use client";
+import { useState } from "react";
+
 import logo from "@/app/assets/ella-logo-black-home-5_65x.webp";
 import Image from "next/image";
 import bag from "@/app/assets/bag.svg";
@@ -5,13 +8,20 @@ import heart from "@/app/assets/heart.svg";
 import search from "@/app/assets/search.svg";
 import burgermenu from "@/app/assets/hamburger.svg";
 import user from "@/app/assets/user.svg";
+import Sidebar from "./sidebar";
 
 const Header = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
   return (
     <div className="text-sm py-5  ">
       <div className="xs:w-11/12 lg:w-10/12 2xl:w-8/12 mx-auto">
         <MainNav />
-        <UtilityNav />
+        <UtilityNav toggleSidebar={toggleSidebar} />
+        <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
       </div>
     </div>
   );
@@ -42,10 +52,10 @@ const MainNav = () => {
   );
 };
 
-const UtilityNav = () => {
+const UtilityNav = ({ toggleSidebar }: { toggleSidebar: () => void }) => {
   return (
     <div className="bg-white left-0 flex items-center text-black w-full justify-between z-20 py-3 px-3 xs:flex lg:hidden">
-      <p>
+      <p onClick={toggleSidebar} className="cursor-pointer">
         <Image src={burgermenu} alt="menu" />
       </p>
       <p>
---
+++ src/app/components/sidebar.tsx
@@ -0,0 +1,59 @@
+import close from "@/app/assets/close.svg";
+import Image from "next/image";
+
+const sidebarItems = [
+  "Home",
+  "New",
+  "Accessories",
+  "Shop This Look",
+  "Blog",
+  "My Wish List",
+  "My Account",
+];
+
+const Sidebar = ({
+  isOpen,
+  toggleSidebar,
+}: {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+}) => {
+  return (
+    <>
+      {/* Overlay */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 z-40"
+          onClick={toggleSidebar} // Closes sidebar when clicked outside
+        ></div>
+      )}
+      {/* siebar */}
+      <div
+        className={`fixed top-0 left-0 h-full w-1/2 bg-white text-black text-lg z-50 transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out`}
+      >
+        <div className="">
+          <div className="sidebar-header bg-black text-white flex justify-between items-center p-4">
+            <div className="  text-lg font-semibold">
+              <p>Menu</p>
+            </div>
+            <p onClick={toggleSidebar} className="cursor-pointer">
+              <Image src={close} alt="close" />
+            </p>
+          </div>
+
+          <ul className="mt-5">
+            {sidebarItems.map((item, index) => (
+              <p key={index} className="py-4 px-4  border-b-2">
+                {item}
+              </p>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
